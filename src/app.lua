bd818234cc9abb9132c54fc2b9704fd3185693ca
Mr Lima <felipebr4095@gmail.com>
2025-10-01 11:58:48 -0300

    Side scroll

--- src/app.lua
+++ src/app.lua
@@ -8,8 +8,9 @@ local function main(filename)
     local f = io.open(filename, "r")
     if f then
         buffer = {}
-        for line in f:lines() do table.insert(buffer, line) end
-
+        for line in f:lines() do
+            table.insert(buffer, line)
+        end
         f:close()
         if #buffer == 0 then buffer = {""} end
     end
@@ -17,18 +18,15 @@ local function main(filename)
     local stdscr = curses.initscr()
     curses.cbreak()
     curses.raw()
-
     if curses.noecho then curses.noecho()
     else curses.echo(false) end
-
     stdscr:keypad(true)
     if stdscr.meta then stdscr:meta(true) end
-
     curses.curs_set(1)
 
     local cx, cy = 0, 0
-    local dirty, saved = false, true
-    local scroll = 0
+    local dirty = false
+    local scroll, hscroll = 0, 0
 
     local function redraw()
         stdscr:clear()
@@ -36,7 +34,6 @@ local function main(filename)
 
         local filename_display = "[" .. filename .. "]"
         if dirty then filename_display = filename_display .. "*" end
-
         local title_left = " TxTLua (" .. version .. ")"
         local filetype = "(text)"
 
@@ -45,28 +42,30 @@ local function main(filename)
         stdscr:mvaddstr(0, math.floor((cols - #filename_display) / 2), filename_display)
         stdscr:mvaddstr(0, cols - #filetype - 1, filetype)
 
-        for i = 1, rows - 1 do
-            local line_index = scroll + i
+        for i = 2, rows do
+            local line_index = scroll + i - 1
             if line_index <= #buffer then
-                stdscr:mvaddstr(i, 0, buffer[line_index]:sub(1, cols))
+                local line = buffer[line_index]
+                if #line > hscroll then stdscr:mvaddstr(i - 1, 0, line:sub(hscroll + 1, hscroll + cols)) end
             end
         end
 
-        stdscr:move(cy - scroll + 1, cx)
+        stdscr:move(cy - scroll + 1, cx - hscroll)
         stdscr:refresh()
     end
-
 
     local function ensure_cursor_visible()
         local rows, cols = stdscr:getmaxyx()
 
         if cy < scroll then scroll = cy
-        elseif cy >= scroll + rows then scroll = cy - rows + 1 end
+        elseif cy >= scroll + rows - 1 then scroll = cy - rows + 2 end
+
+        if cx < hscroll then hscroll = cx
+        elseif cx >= hscroll + cols then hscroll = cx - cols + 1  end
     end
 
     local function insert_char(ch)
         local line = buffer[cy + 1]
-
         buffer[cy + 1] = line:sub(1, cx) .. ch .. line:sub(cx + 1)
         cx = cx + 1
         dirty = true
@@ -105,34 +104,26 @@ local function main(filename)
         local ch = stdscr:getch()
 
         if ch == 3 then
-            if dirty then
-                stdscr:clear()
-                stdscr:mvaddstr(0, 0, "Save? [Y] yes, [N] no, [B] back: ")
-                stdscr:refresh()
-
-                local answer = stdscr:getch()
-
-                if answer == string.byte("Y") or answer == string.byte("y") then
-                    curses.endwin()
-                    if dirty then
-                        print("Writing on '" .. filename .. "'...")
-                        f = io.open(filename, "w")
-                        if f then
-                            for _, line in ipairs(buffer) do f:write(line .. "\n") end
-                            f:close()
-                        end
-                    end
-
-                    break
-                elseif answer == string.byte("N") or answer == string.byte("n") then
-                    curses.endwin()
-                    print("Discard.")
-                    break
-                else redraw() end
-            else
+            stdscr:clear()
+            stdscr:mvaddstr(0, 0, "Save? [Y] yes, [N] no, [B] back: ")
+            stdscr:refresh()
+            local answer = stdscr:getch()
+            if answer == string.byte("Y") or answer == string.byte("y") then
                 curses.endwin()
+                if dirty then
+                    print("Writing on '" .. filename .. "'...")
+                    f = io.open(filename, "w")
+                    if f then
+                        for _, line in ipairs(buffer) do f:write(line .. "\n") end
+                        f:close()
+                    end
+                end
                 break
-            end
+            elseif answer == string.byte("N") or answer == string.byte("n") then
+                curses.endwin()
+                print("Trashed.")
+                break
+            else redraw() end
         elseif ch == 19 then
             f = io.open(filename, "w")
             if f then
@@ -140,7 +131,6 @@ local function main(filename)
                 f:close()
                 dirty = false
                 redraw()
-
                 stdscr:mvaddstr(0, 0, " TxTLua (" .. version .. ") - Saved!")
                 stdscr:refresh()
                 curses.napms(500)
@@ -156,13 +146,11 @@ local function main(filename)
         elseif ch == curses.KEY_UP then
             if cy > 0 then
                 cy = cy - 1
-
                 if cx > #buffer[cy + 1] then cx = #buffer[cy + 1] end
             end
         elseif ch == curses.KEY_DOWN then
             if cy < #buffer - 1 then
                 cy = cy + 1
-
                 if cx > #buffer[cy + 1] then cx = #buffer[cy + 1] end
             end
         elseif ch >= 32 and ch <= 126 then insert_char(string.char(ch)) end
